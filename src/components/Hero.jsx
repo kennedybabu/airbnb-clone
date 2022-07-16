3f021e7b0658fd2bfc59e3be8081dae528fe13a6
kennedybabu <kennedynjogu8@gmail.com>
2022-07-16 08:57:21 +0300

    style the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Images from "../assets/photo-grid.png"
 
 const Hero = () => {
   return (
-    <div>
-        hero
+    <div className='w-full h-[300px] px-[36px]'>
+        {/* container */}
+        <div className='w-full h-full flex items-center justify-center'>
+            <div className='w-[366px] h-[232px]'>
+                <img src={Images} alt="hero photo grid" className='h-full w-full'/>
+            </div>          
+        </div>  
+        <h1 className='text-[36px] font-bold'>Online Experiences</h1>
+        <p className='text-[16px] text-[#222222] max-w-[320px]'>Join unique interactive activities led by one-of-kind hosts-all without leaving home.</p>      
     </div>
   )
 }
